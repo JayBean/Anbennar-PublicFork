f4a4f9cee3b3f75dc418983d80aeab4c6f0abe95
mm3u19 <mm3u19@soton.ac.uk>
2023-02-24 19:22:44 +0000

    In theory working

--- history/_natives.py
+++ history/_natives.py
@@ -33,8 +33,9 @@ def PrintDictDict(culturesDict) :
                 a = 1
 
 class Province:
-    def __init__(self, terrain, pathName):
+    def __init__(self, terrain, provinceID, pathName):
         self.terrain = terrain
+        self.provinceID = provinceID
         self.pathName = pathName
         self.isTribal = False
         encoding = GetEncoding(directory + "provinces/" + pathName)
@@ -95,30 +96,24 @@ for file_name in os.listdir((directory + "provinces/")[:-1]):
     # Check if the file is a regular file (i.e., not a directory)
     if os.path.isfile(os.path.join((directory + "provinces/")[:-1], file_name)):
         # Add the file name to the list
-        dict_province.update({file_name.split("-")[-1].strip().split(".")[0] : file_name})
+        dict_province.update({file_name.split("-")[0].strip() : file_name})
 
-ToFileDicts(dict_province)
+#ToFileDicts(dict_province)
 
 encoding = GetEncoding(directory + "_provinces.txt")
 
 #Open same file, this time using the proper encoding.
-with open(directory + "_provinces.txt", "r", encoding=encoding) as file:
-    #Special characters dict
-    special_chars = {"â": "a", "î": "i", "ô": "o", "è": "e", "ï": "i", "á": "a", "é": "e", "í": "i", "ó": "o", "ú": "u", "à": "a", "ù": "u", "ê": "e", "û": "u", "ç": "c"}
+with open(directory + "_provincesNew.txt", "r", encoding=encoding) as file:
 
     for line in file:
-        for special_char, regular_char in special_chars.items():
-            #remove special characters and apostrophes
-            line = line.replace(special_char, regular_char)
-            line = line.replace("'", "")
-        province = line.split(":", 3)[3].strip()
-        province_split = province.split(" ")
-        if dict_province.__contains__('_'.join(map(str,province_split[1:-2]))) : 
-            listProvinces.append(Province(province_split[-1], dict_province.get('_'.join(map(str,province_split[1:-2])))))
-        province = province_split[0] + " " + '_'.join(map(str,province_split[1:-2])) + " " + ' '.join(province_split[-2:])
-        province_array.append(province)
+        infoLine = line.split(":", 3)[3].strip()
+        province_split = infoLine.split("|")
+        if dict_province.__contains__(province_split[0]) : 
+            listProvinces.append(Province(province_split[1], province_split[0], dict_province.get(province_split[0])))
+        #province = province_split[0] + " " + '_'.join(map(str,province_split[1:-2])) + " " + ' '.join(province_split[-2:])
+        #province_array.append(province)
 
-ToFileTerrains(province_array)
+#ToFileTerrains(province_array)
 
 for pr in listProvinces :
     #pr.printSelf()
@@ -147,5 +142,4 @@ for pr in listProvinces :
         with open(directory + "provinces/" + pr.pathName, "w") as file:
             file.writelines(data)
             print("\n".join(data))
-    break
         
